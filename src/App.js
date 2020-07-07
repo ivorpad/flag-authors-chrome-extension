8e8c097f06682a619dab6b2c9f17abb82296625e
Ivor Padilla <ivorjpc@gmail.com>
2020-07-07 16:45:10 +0200

    initial commit - next steps: refactor

--- src/App.js
+++ src/App.js
@@ -1,24 +1,184 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import { useQuery, useMutation, queryCache } from "react-query";
+import "./App.css";
+import { request } from "graphql-request";
+import { v4 as uuidv4 } from "uuid";
+import Linkify from "react-linkify";
+
+const query = `query($author: String!) {
+  reasons(orderBy: {createdAt: desc}, where: { authorId: { equals: $author } }) {
+    id
+    reviewerId
+    reason
+    authorId
+  }
+}`;
+
+const mutation = `mutation($reason:String! $authorId: String! $reviewerId: String!) {
+  createOneReason(reason: $reason, authorId: $authorId, reviewerId: $reviewerId) {
+    reason
+    authorId
+    reviewerId
+    id
+  }
+}`;
+
+const DELETE_ITEM = `mutation($id: Int!) {
+  deleteOneReason(where: { id: $id }) {
+    reason
+    id
+  }
+}`; 
 
 function App() {
+
+  const [pageData, setPageData] = useState({})
+  const [reason, setReason] = useState("")
+  const [isVisible, setIsVisible] = useState(false)
+
+  useEffect(() => {
+
+    const authorId = document
+      .querySelector(".submission-details > div")
+      .querySelectorAll("a")[0].innerText;
+
+    const reviewerId = document
+      .getElementById("spec-user-username")
+      .innerText.toLowerCase();
+  
+      setPageData({
+        authorId,
+        reviewerId,
+      });
+
+  }, []);
+
+  async function getData() {
+    const author = document
+      .querySelector(".submission-details > div")
+      .querySelectorAll("a")[0].innerText;
+
+    return request(`https://geo.ivorpad.com/graphql`, query, { author })
+      .then((data) => data)
+      .catch((err) => err);
+  }
+
+  async function mutateData(variables) {
+    return request(`https://geo.ivorpad.com/graphql`, mutation, variables)
+      .then((data) => data)
+      .catch((err) => err);
+  }
+
+  async function deleteItemMutation(variables) {
+    return request(`https://geo.ivorpad.com/graphql`, DELETE_ITEM, variables)
+      .then((data) => data)
+      .catch((err) => err);
+  }
+
+  const { data, isLoading } = useQuery("reasons", getData);
+  const [mutate] = useMutation(mutateData, {
+    onMutate: (newReason) => {
+      const previousReasons = queryCache.cancelQueries("reasons");
+      queryCache.setQueryData("reasons", (old) => {
+        return { reasons: [newReason, ...old.reasons] };
+      });
+      return () => queryCache.setQueryData("reasons", previousReasons);
+    },
+    onError: (err, newReason, rollback) => {
+      rollback();
+    },
+    onSettled: () => {
+      queryCache.invalidateQueries("reasons");
+    },
+  });
+
+  const [deleteItem] = useMutation(deleteItemMutation, {
+    onMutate: (item) => {
+      const previousReasons = queryCache.cancelQueries("reasons");
+      queryCache.setQueryData("reasons", (old) => {
+        return {
+          reasons: old.reasons.filter((reason) => reason.id !== item.id),
+        };
+      });
+      return () => queryCache.setQueryData("reasons", previousReasons);
+    }
+  });
+
+  if (isLoading) return <p>Loading...</p>;
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button
+        className="flag-authors"
+        onClick={(e) => {
+          e.preventDefault();
+          setIsVisible(() => !isVisible);
+        }}>
+        {isVisible ? "× Close" : "Flag Author"}
+      </button>
+      {isVisible && (
+        <form
+          onSubmit={(e) => {
+            e.preventDefault();
+            const variables = {
+              reason,
+              authorId: pageData.authorId,
+              reviewerId: pageData.reviewerId,
+            };
+            mutate(variables, {
+              onError: (e) => console.log("error", e),
+              onSuccess: (e) => setIsVisible(false),
+            });
+            setReason("");
+          }}>
+          <textarea
+            onChange={(e) => {
+              setReason(e.target.value);
+            }}
+            name="reason"
+            id="reason"
+            cols="30"
+            value={reason}
+            rows="10"></textarea>
+          <button type="submit">Send Reason</button>
+        </form>
+      )}
+      <ul className="reasons-form">
+        {Object.values(data).length > 0 && data.reasons?.length === 0 ? (
+          <p>No reports</p>
+        ) : (
+          data.reasons.map(({ reason, id }) => (
+            <li key={uuidv4()}>
+              <Linkify>{reason}</Linkify>
+              <small
+                style={{
+                  textDecoration: "underline",
+                  cursor: "pointer",
+                  color: "blue",
+                  marginLeft: ".5rem",
+                }}
+                onClick={(e) => {
+                  e.preventDefault();
+                }}>
+                [edit]
+              </small>
+              <small
+                style={{
+                  textDecoration: "underline",
+                  cursor: "pointer",
+                  color: "red",
+                  marginLeft: ".5rem",
+                }}
+                onClick={(e) => {
+                  e.preventDefault();
+                  deleteItem({ id: +id });
+                }}>
+                [×]
+              </small>
+            </li>
+          ))
+        )}
+      </ul>
     </div>
   );
 }
